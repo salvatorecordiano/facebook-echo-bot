c55a1866746f85748bff95b564ce1012b623aa64
Salvatore Cordiano <salvatorecordiano@users.noreply.github.com>
2020-04-07 23:03:54 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('FACEBOOK_PAGE_ACCESS_TOKEN', 'Insert here');
 define('FACEBOOK_PAGE_ID', 'Insert here');
 define('FACEBOOK_VALIDATION_TOKEN', 'Insert here');
+define('FACEBOOK_PAGE_ACCESS_TOKEN', 'Insert here');
